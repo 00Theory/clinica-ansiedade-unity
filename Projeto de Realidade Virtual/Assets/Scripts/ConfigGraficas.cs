ba8a14e82b98ba640ae27c7d37f0338f21ce8fff
00Theory <marco200002010@outlook.com>
2023-08-10 21:14:57 -0300

    Compartilhando projeto

---
+++ Projeto de Realidade Virtual/Assets/Scripts/ConfigGraficas.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigGraficas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Screen.orientation = ScreenOrientation.AutoRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Projeto de Realidade Virtual/Builds Android/exemplo1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ConfigGraficas::Awake()
+extern void ConfigGraficas_Awake_mE258A51BF6D95CF1E200E09A5BE41B6A88FCDEA9 (void);
+// 0x00000002 System.Void ConfigGraficas::Update()
+extern void ConfigGraficas_Update_m2F1954A48D6EA71EF6128ED487B4857126ECD0B2 (void);
+// 0x00000003 System.Void ConfigGraficas::.ctor()
+extern void ConfigGraficas__ctor_m254DCB6F8C89F94118EE87C4415A2766DBF87178 (void);
+// 0x00000004 System.Void Unity.Template.VR.XRPlatformControllerSetup::Start()
+extern void XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF (void);
+// 0x00000005 System.Void Unity.Template.VR.XRPlatformControllerSetup::.ctor()
+extern void XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	ConfigGraficas_Awake_mE258A51BF6D95CF1E200E09A5BE41B6A88FCDEA9,
+	ConfigGraficas_Update_m2F1954A48D6EA71EF6128ED487B4857126ECD0B2,
+	ConfigGraficas__ctor_m254DCB6F8C89F94118EE87C4415A2766DBF87178,
+	XRPlatformControllerSetup_Start_m1F22FCA29DFD83DC0E343C3F391D04A7C52085BF,
+	XRPlatformControllerSetup__ctor_mF9A3998AF90962CF8F35BAF2221558BDF5F6596E,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	4762,
+	4762,
+	4762,
+	4762,
+	4762,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
